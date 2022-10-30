68a9ff1dabfe7795fd8c4cbb3b4629aec7f76517
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-10-30 21:38:49 +0300

    #2 Add documentation stub to root package

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package flow provides core concepts of data flow from emitter to collector
+// with support of error handling and possible cancellation of the flow via
+// context mechanisms.
+package flow
