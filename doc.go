a3c01f8578447c5ce568067eb98555cd549c8b67
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-10-30 21:41:45 +0300

    #2 Add links to articles to documentation of root package

--- doc.go
+++ doc.go
@@ -1,4 +1,8 @@
 // Package flow provides core concepts of data flow from emitter to collector
 // with support of error handling and possible cancellation of the flow via
-// context mechanisms.
+// context mechanisms. The implementation is inspired by articles in [Go blog]
+// and [Medium].
+//
+// [Go blog]: https://go.dev/blog/pipelines
+// [Medium]: https://medium.com/amboss/applying-modern-go-concurrency-patterns-to-data-pipelines-b3b5327908d4
 package flow
