ceae14929dddbb37f8362bbf92a7c5f21c15f32a
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-11-04 20:32:49 +0300

    #7 Add One type of arrays of error-writing channels for regular cases

--- errors/chans.go
+++ errors/chans.go
@@ -3,3 +3,7 @@ package errors
 // No is type of zero-elements arrays of error-writing channels, effectively
 // meaning that its users are simple enough to need no error handling.
 type No [0]chan<- error
+
+// One is type of one-element arrays of error-writing channels used by regular
+// cases.
+type One [1]chan<- error
