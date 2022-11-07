cf5c919f5ee3929eb4886f4c9180913d1bd8ba65
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-11-07 07:05:07 +0300

    #12 Introduce new values/receiver subpackage

---
+++ values/receiver/doc.go
@@ -0,0 +1,4 @@
+// Package receiver provides implementation of [values.Receiver] interface,
+// which is founded on reading from channel within context, and factory
+// function to create it.
+package receiver
