206cf976df709cc16113796ed85fd69c3360ec22
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-11-13 12:44:18 +0300

    #12 Fix typo in documentation on emitters.Producer

--- emitters/producer.go
+++ emitters/producer.go
@@ -11,8 +11,8 @@ import (
 // implementations.
 type Producer[V any, E errors.Senders] interface {
 	// Produce should produce values of V type and send them with the
-	// provided sender. Depending on the provided collection of
-	// error-senders of type from set E, it can also reporting on errors.
-	// It can assume, that the provided instances are nevel nil.
+	// provided sender. Depending on the provided collection of error
+	// senders of type from set E, it can also report on errors. It can
+	// assume, that the provided instances are nevel nil.
 	Produce(context.Context, values.Sender[V], E)
 }
