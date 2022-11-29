ad18c6ea66312bf9429e01e8d283fff411a48a93
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-11-29 18:44:49 +0300

    #18 Introduce pit subpackage

---
+++ pit/doc.go
@@ -0,0 +1,5 @@
+// Package pit provides description of data flow, which originates from kind of
+// source. The description separates production of values (_mining_ in terms of
+// the package) from their transporting, and allows to create data flows with
+// help of the producers' implementations.
+package pit
