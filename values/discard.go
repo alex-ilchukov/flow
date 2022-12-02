d9d127509fa7728c6c1cfcf5988b4173671ed363
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-12-02 15:04:40 +0300

    #26 Remove Discard function from values subpackage

--- values/discard.go
+++
@@ -1,8 +0,0 @@
-package values
-
-// Discard reads values of type V in the blocking way from the provided channel
-// til the channel is closed. It does nothing with the values.
-func Discard[V any](c <-chan V) {
-	for range c {
-	}
-}
