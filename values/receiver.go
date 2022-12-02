7972ae05bc5b92b89a1fd62ffd8deaf5e844a80e
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-12-02 14:01:58 +0300

    #24 Remove Receiver interface from values subpackage

--- values/receiver.go
+++
@@ -1,9 +0,0 @@
-package values
-
-// Receiver is abstract type of entities which receive values of type V.
-type Receiver[V any] interface {
-	// Receive should try to receive value of V type. It should return the
-	// value and nil, if no error appears, or default value and the error
-	// otherwise. It must return [Over] if receiving is impossible anymore.
-	Receive() (V, error)
-}
