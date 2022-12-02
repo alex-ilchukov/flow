fedf013f7b46468f661a9eb3b93759ac1729b825
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-12-02 14:00:52 +0300

    #24 Remove receiver subpackage

--- values/receiver/doc.go
+++
@@ -1,4 +0,0 @@
-// Package receiver provides implementation of [values.Receiver] interface,
-// which is founded on reading from channel within context, and factory
-// function to create it.
-package receiver
